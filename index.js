a72883b7e71db96222804e3825f6b2d93aad99b3
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-10 14:52:42 -0500

    Add PuppyListItem & PuppyList components

--- index.js
+++ index.js
@@ -4,9 +4,34 @@ const COHORT = "/2604-JAMES";
 const API = BASE + COHORT;
 
 // === State ===
-let players = [];
-let selectedPlayer;
+let puppies = [];
+let selectedPuppy;
 
 // === Components ===
 
+/** I need a function to create a list item that provides player details when clicked */
+function PuppyListItem(puppy) {
+  const $li = document.createElement("li");
+
+  if (puppy.id === selectedPuppy?.id) {
+    $li.classList.add("selected");
+  }
+
+  $li.innerHTML = `
+        <a href="selected">${puppy.name}</a>
+    `;
+
+  return $li;
+}
+
+/** I need a function to create an unordered list to contain my list items */
+function PuppyList() {
+  const $ul = document.createElement("ul");
+  $ul.classList.add("puppies");
+
+  const $puppies = puppies.map(PuppyListItem);
+  $ul.replaceChildren(...$puppies);
+
+  return $ul;
+}
 // === Render ===
